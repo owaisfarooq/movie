3b431fde178789ab4a02c93a22773b60fc8175a1
owaisfarooq <owais.farooq.qureshi@gmail.com>
2023-08-19 23:47:49 +0500

    cloned from movieApis

---
+++ public/script.js
@@ -0,0 +1,97 @@
+const url = "http://localhost:3001"
+
+async function getAnimes() {
+    fetch(url + '/getAllAnime/', {
+            method: 'POST'
+        })
+        .then(res => res.json())
+        .then(json => {
+            if (json.status !== 0) {
+                alert(`ERROR: status: ${json.status} messgage: ${json.message}`)
+            }
+            console.table(json.data);
+            const data = json.data
+            const tableBody = document.getElementById("tableBody");
+            tableBody.innerHTML = "";
+            for (let index = 0; index < data.length; index++) {
+                tableBody.innerHTML += makeRows(data[index])
+            }
+            return json
+        })
+}
+async function addNewAnime () {
+    const animeNameField = document.getElementById("animeNameField");
+    fetch(url + '/saveAnime', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            animeName: animeNameField.value,
+        })
+    })
+    .then(res => res.json())
+    .then(json => {
+        displayData ( json )
+        animeNameField.value = ""
+        return json
+    })
+}
+function displayData ( jsonData ) {
+    const data = jsonData.data
+    const tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = "";
+
+    for (let index = 0; index < data.length; index++) {
+        tableBody.innerHTML += makeRows(data[index])
+    }
+}
+async function search ( element ) {
+    const searchStr = element.value.toUpperCase()
+    fetch(url + '/getAllAnime', {
+        method: "POST",
+        body: JSON.stringify()
+    })
+    .then(res => res.json())
+    .then(json => {
+        
+        displayData({data: json.data.filter ( v => {
+            return v.animeName.toUpperCase().includes(searchStr)
+        })})
+    })
+    
+}
+function deleteAnime(animeName) {
+    fetch(url + '/deleteAnime/', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            animeName: animeName,
+        })
+    })
+
+    .then(res => res.json())
+    .then(json => {
+        displayData ( json )
+        return json
+    })
+}
+
+function makeRows(animeObject) {
+    return `
+        <tr>
+            <td>${animeObject.animeName}</td>
+            <td><button onclick="deleteAnime('${animeObject.animeName}')" class="btn btn-danger">Delete</button></td>
+        </tr>
+    `
+}
+
+async function initializePage() {
+    getAnimes()
+}
+
+initializePage()
